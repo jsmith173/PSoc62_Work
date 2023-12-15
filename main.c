2700fd1ecce2b5b575f4a066d72c2fba5901e1d1
attx <attx@localhost>
2023-12-15 13:45:30 +0100

    PWM=1Hz

--- main.c
+++ main.c
@@ -1,51 +1,20 @@
 #include "cyhal.h"
 #include "cybsp.h"
 
-bool LEDupdateFlag = false;
 
-/* Isr_Timer configuration structure*/
-cy_stc_sysint_t Isr_Timer_config = {
-	.intrSrc = (IRQn_Type) CYBSP_TIMER_IRQ,
-	.intrPriority = 7u
-};
-
-void Isr_Timer(void)
+void PWMInit(void)
 {
-	
-	volatile uint32_t interrupts = Cy_TCPWM_GetInterruptStatus(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
-    /* Now the 'interrupts' contains all the currently pending interrupt masks */
-	
-    if (CY_TCPWM_INT_ON_TC & interrupts)
-    {
-		/* Clear the TCPWM peripheral interrupt */
-		Cy_TCPWM_ClearInterrupt(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, CY_TCPWM_INT_ON_TC);
-    }
-    if (CY_TCPWM_INT_ON_CC0 & interrupts)
-    {
-		/* Clear the TCPWM peripheral interrupt */
-		Cy_TCPWM_ClearInterrupt(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, CY_TCPWM_INT_ON_CC0);
-    }
-    volatile uint32_t cnt = TCPWM_CNT_COUNTER(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
-	
-	/* Clear the CM4 NVIC pending interrupt for TCPWM */
-	NVIC_ClearPendingIRQ(Isr_Timer_config.intrSrc);
-	LEDupdateFlag = true;
-}
-
-void TimerInit(void)
-{
-	/* Configure the TCPWM peripheral.
-	Counter_config structure is defined based on the parameters entered
-	in the Component configuration */
-	Cy_TCPWM_Counter_Init(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, &CYBSP_TIMER_config);
-	/* Enable the initialized counter */
-	Cy_TCPWM_Counter_Enable(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
-	/* Start the enabled counter */
-	Cy_TCPWM_TriggerStart(CYBSP_TIMER_HW, CYBSP_TIMER_MASK);
-	/* Configure the ISR for the TCPWM peripheral*/
-	Cy_SysInt_Init(&Isr_Timer_config, Isr_Timer);
-	/* Enable interrupt in NVIC */
-	NVIC_EnableIRQ((IRQn_Type)Isr_Timer_config.intrSrc);
+    cy_rslt_t   rslt;
+    cyhal_pwm_t pwm_obj;
+ 
+    // Initialize PWM on the supplied pin and assign a new clock
+    rslt = cyhal_pwm_init(&pwm_obj, CYBSP_USER_LED, NULL);
+ 
+    // Set a duty cycle of 50% and frequency of 1Hz
+    rslt = cyhal_pwm_set_duty_cycle(&pwm_obj, 50, 1);
+ 
+    // Start the PWM output
+    rslt = cyhal_pwm_start(&pwm_obj);
 }
 
 int main(void)
@@ -73,7 +42,7 @@ int main(void)
     /* Enable global interrupts */
     __enable_irq();
 	
-	TimerInit();
+	PWMInit();
 
     for (;;)
     {
