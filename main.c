6c32f0c1667e32d66eb68d114372f362e52db11b
attx <attx@localhost>
2023-12-15 09:45:56 +0100

    tcpwm interrupt is OK

--- main.c
+++ main.c
@@ -1,85 +1,53 @@
-/*******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the Empty Application Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*
-********************************************************************************
-* Copyright 2021-2023, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
-/*******************************************************************************
-* Header Files
-*******************************************************************************/
 #include "cyhal.h"
 #include "cybsp.h"
 
-/*******************************************************************************
-* Macros
-*******************************************************************************/
+bool LEDupdateFlag = false;
 
+/* Isr_Timer configuration structure*/
+cy_stc_sysint_t Isr_Timer_config = {
+	.intrSrc = (IRQn_Type) CYBSP_TIMER_IRQ,
+	.intrPriority = 7u
+};
 
-/*******************************************************************************
-* Global Variables
-*******************************************************************************/
+void Isr_Timer(void)
+{
+	
+	volatile uint32_t interrupts = Cy_TCPWM_GetInterruptStatus(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
+    /* Now the 'interrupts' contains all the currently pending interrupt masks */
+	
+    if (CY_TCPWM_INT_ON_TC & interrupts)
+    {
+		/* Clear the TCPWM peripheral interrupt */
+		Cy_TCPWM_ClearInterrupt(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, CY_TCPWM_INT_ON_TC);
+    }
+    if (CY_TCPWM_INT_ON_CC0 & interrupts)
+    {
+		/* Clear the TCPWM peripheral interrupt */
+		Cy_TCPWM_ClearInterrupt(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, CY_TCPWM_INT_ON_CC0);
+    }
+    volatile uint32_t cnt = TCPWM_CNT_COUNTER(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
+	
+	/* Clear the CM4 NVIC pending interrupt for TCPWM */
+	NVIC_ClearPendingIRQ(Isr_Timer_config.intrSrc);
+	LEDupdateFlag = true;
+}
 
+void TimerInit(void)
+{
+	/* Configure the TCPWM peripheral.
+	Counter_config structure is defined based on the parameters entered
+	in the Component configuration */
+	Cy_TCPWM_Counter_Init(CYBSP_TIMER_HW, CYBSP_TIMER_NUM, &CYBSP_TIMER_config);
+	/* Enable the initialized counter */
+	Cy_TCPWM_Counter_Enable(CYBSP_TIMER_HW, CYBSP_TIMER_NUM);
+	/* Start the enabled counter */
+	Cy_TCPWM_TriggerStart(CYBSP_TIMER_HW, CYBSP_TIMER_MASK);
+	/* Configure the ISR for the TCPWM peripheral*/
+	Cy_SysInt_Init(&Isr_Timer_config, Isr_Timer);
+	/* Enable interrupt in NVIC */
+	NVIC_EnableIRQ((IRQn_Type)Isr_Timer_config.intrSrc);
+}
 
-/*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Definitions
-*******************************************************************************/
-
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
 int main(void)
 {
     cy_rslt_t result;
@@ -104,10 +72,11 @@ int main(void)
 
     /* Enable global interrupts */
     __enable_irq();
+	
+	TimerInit();
 
     for (;;)
     {
     }
 }
 
-/* [] END OF FILE */
